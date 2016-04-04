c8f53813fa2cbdfe72d09e21be4a1ba719045d07
DarkAce65 <tvdood99@gmail.com>
2016-04-04 10:19:32 -0400

    Add inital helper methods

---
+++ client/clientCode.js
@@ -0,0 +1,12 @@
+Template.registerHelper("contains", function(item, array) {
+	return array.indexOf(item) !== -1;
+});
+
+Template.gamelist.helpers({
+	"games": function() {
+		return Games.find();
+	},
+	"numPlayers": function() {
+		return this.gameData.players.length;
+	}
+});
