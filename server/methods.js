1cc325b9312bfa1b2af94aae28fa209874ca8374
DarkAce65 <tvdood99@gmail.com>
2016-04-05 13:35:10 -0400

    Write createGame method

--- server/methods.js
+++ server/methods.js
@@ -1,3 +1,27 @@
 Meteor.methods({
+	"createGame": function(gameType, lobbyName, password) {
+		if(!GAME_DEFINITIONS.hasOwnProperty(gameType)) {
+			throw new Meteor.Error("unrecognized-game", "A game of the specified type is not defined.");
+		}
+		if(Games.findOne({"lobbbyData.lobbyName": lobbyName})) {
+			throw new Meteor.Error("lobby-name-taken", "A lobby with this name already exists.");
+		}
+		var game = {
+			"gameType": gameType,
+			"inGame": false,
+			"password": false,
+			"lobbyData": {
+				"lobbyName": lobbyName,
+				"players": [this.userId],
+				"minPlayers": GAME_DEFINITIONS[gameType].minPlayers,
+				"maxPlayers": GAME_DEFINITIONS[gameType].maxPlayers
+			},
+			"gameData": GAME_DEFINITIONS[gameType].gameData
+		};
+		if(password) {
+			game.password = password;
+		}
 
+		Games.insert(game);
+	}
 });
