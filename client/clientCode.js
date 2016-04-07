59593efe266a14023c9d62dd8a778d1f6d4dae20
DarkAce65 <tvdood99@gmail.com>
2016-04-07 13:40:27 -0400

    Create joinLobby click event

--- client/clientCode.js
+++ client/clientCode.js
@@ -10,3 +10,47 @@ Template.gamelist.helpers({
 		return this.lobbyData.players.length;
 	}
 });
+
+Template.gamelist.events({
+	"click .joinLobby": function(e) {
+		var button = $(e.target);
+		if(!button.hasClass("visibleInput")) {
+			$(".showInput, .visibleInput").removeClass("showInput visibleInput");
+			$(".joinLobby").html("Join");
+		}
+
+		var lobbyId = this._id;
+		var lobbyName = this.lobbyData.lobbyName;
+
+		if(this.lobbyData.private) {
+			button.html("Enter");
+			var lobbyPassword = button.closest(".lobby").find(".lobbyPassword");
+			lobbyPassword.addClass("showInput");
+
+			if(button.hasClass("visibleInput")) {
+				Meteor.call("joinLobby", lobbyName, lobbyPassword.find("input").val(), function(error) {
+					if(error) {
+						console.log(error.message);
+					}
+					else {
+						Router.go("game", {_id: lobbyId});
+					}
+				});
+				lobbyPassword.find("input").val("");
+			}
+			else {
+				button.addClass("visibleInput");
+			}
+		}
+		else {
+			Meteor.call("joinLobby", lobbyName, function(error) {
+				if(error) {
+					console.log(error.message);
+				}
+				else {
+					Router.go("game", {_id: lobbyId});
+				}
+			});
+		}
+	}
+});
