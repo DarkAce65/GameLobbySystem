43783ec39960fce52f5c977a85a488e98a8c06c1
DarkAce65 <tvdood99@gmail.com>
2016-04-13 18:36:36 -0400

    Remove unused event handler

--- client/clientCode.js
+++ client/clientCode.js
@@ -102,45 +102,7 @@ Template.searchGames.events({
 });
 
 Template.gamelist.events({
-	"click .joinLobby": function(e) {
-		var button = $(e.target);
-		if(!button.hasClass("visibleInput")) {
-			$(".showInput, .visibleInput").removeClass("showInput visibleInput");
-			$(".joinLobby").html("Join");
-		}
+	"click .cover": function(e) {
 
-		var lobbyId = this._id;
-		var lobbyName = this.lobbyData.lobbyName;
-
-		if(this.lobbyData.private) {
-			button.html("Enter");
-			var lobbyPassword = button.closest(".lobby").find(".lobbyPassword");
-			lobbyPassword.addClass("showInput");
-
-			if(button.hasClass("visibleInput")) {
-				Meteor.call("joinLobby", lobbyName, lobbyPassword.find("input").val(), function(error) {
-					if(error) {
-						console.log(error.message);
-					}
-					else {
-						Router.go("lobby", {_id: lobbyId});
-					}
-				});
-				lobbyPassword.find("input").val("");
-			}
-			else {
-				button.addClass("visibleInput");
-			}
-		}
-		else {
-			Meteor.call("joinLobby", lobbyName, function(error) {
-				if(error) {
-					console.log(error.message);
-				}
-				else {
-					Router.go("lobby", {_id: lobbyId});
-				}
-			});
-		}
 	}
 });
