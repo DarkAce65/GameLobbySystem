891812defefdaf74790942e57cbe91daddaeca54
DarkAce65 <tvdood99@gmail.com>
2016-04-18 18:04:27 -0400

    Standardize code style

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # This script will run the app in development with the correct settings.
 
 if [ ! -f settings.json ]; then
-    echo 'settings.json not found - run prepDevelopment.sh and fill out keys'
-    exit 1
+	echo "settings.json not found - run prepDevelopment.sh and fill out keys"
+	exit 1
 fi
 
 meteor --settings=settings.json
--- server/oAuth.js
+++ server/oAuth.js
@@ -1,13 +1,14 @@
 var configure = function(services) {
-  if (services) {
-    for (service in services) {
-      ServiceConfiguration.configurations.upsert( { service: service }, {
-        $set: services[ service ]
-      });
-    }
-  } else {
-    throw new Meteor.Error('bad-oauth-configuration', 'No services defined')
-  }
+	if(services) {
+		for(var service in services) {
+			ServiceConfiguration.configurations.upsert({service: service}, {
+				$set: services[service]
+			});
+		}
+	}
+	else {
+		throw new Meteor.Error("bad-oauth-configuration", "No services defined");
+	}
 }
 
-configure(Meteor.settings.private.oAuth)
+configure(Meteor.settings.private.oAuth);
