14d3040aa84f001ee417a7c2c5fb895ee53e73f9
Karim Fatehi <k.electron@gmail.com>
2016-04-17 23:47:37 -0400

    Make oauth services configuration driven and add a logout button.

--- server/oAuth.js
+++ server/oAuth.js
@@ -1,2 +1,13 @@
-console.log(Meteor.settings.private.oAuth)
-//TODO: add actual oAuth service configuration
+var configure = function(services) {
+  if (services) {
+    for (service in services) {
+      ServiceConfiguration.configurations.upsert( { service: service }, {
+        $set: services[ service ]
+      });
+    }
+  } else {
+    throw new Meteor.Error('bad-oauth-configuration', 'No services defined')
+  }
+}
+
+configure(Meteor.settings.private.oAuth)
